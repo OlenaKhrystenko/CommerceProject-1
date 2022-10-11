e2afc3079a234dc192433359163bdebc5e96df81
OlenaKhrystenko <ovkncp@umsystem.edu>
2022-10-11 14:58:02 -0500

    New models, views, and controllers.

---
+++ Controllers/DonationController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CommerceProject.Controllers
+{
+    public class DonationController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult MakeDonation() { 
+            //to do
+            return View("DonationForm");
+        }
+    }
+}
---
+++ Models/CardInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CommerceProject.Models
+{
+    public class CardInfo
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string CardHolderName { get; set; }
+        [Required]
+        public string CardNumber { get; set; }
+        [Required]
+        public string ExpirationDate { get; set; }
+        [Required]
+        public int CVV { get; set; }
+
+    }
+}
