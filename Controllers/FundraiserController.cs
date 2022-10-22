b46d06f0cbb972eaa0a4745ec2d184c18c925ac9
OlenaKhrystenko <ovkncp@umsystem.edu>
2022-10-22 13:23:02 -0500

    Started working on Fundraisers. Added new option "Fundraisers".

---
+++ Controllers/FundraiserController.cs
@@ -0,0 +1,28 @@
+﻿using CommerceProject.Data;
+using CommerceProject.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CommerceProject.Controllers
+{
+    public class FundraiserController : Controller
+    {
+        private readonly ApplicationDBContext _db;
+
+        public FundraiserController(ApplicationDBContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult FundraiserListView() 
+        {
+            IEnumerable<Fundraiser> objFundraiserList = _db.Fundraisers;
+
+            return View(objFundraiserList);
+        }
+    }
+}
