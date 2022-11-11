631d82eaf79231af489de5412e7f64b41bfd713f
OlenaKhrystenko <ovkncp@umsystem.edu>
2022-11-11 13:19:46 -0600

    passing model data between views

--- Controllers/FundraiserController.cs
+++ Controllers/FundraiserController.cs
@@ -1,6 +1,7 @@
 ﻿using CommerceProject.Data;
 using CommerceProject.Models;
 using Microsoft.AspNetCore.Mvc;
+using System.Xml.Linq;
 
 namespace CommerceProject.Controllers
 {
@@ -15,13 +16,29 @@ namespace CommerceProject.Controllers
 
         public IActionResult Index()
         {
+            return View(new Fundraiser_1());
+        }
+
+        [HttpPost]
+        public ContentResult Index(Fundraiser_1 objFund) 
+        {
+            return Content("Fundraiser Name: " + objFund.FundraiserName + " " + objFund.FundraiserDescription);
+        }
+
+        public IActionResult ShowData(string FundraiserName, string Description, double CurrentAmount, double Goal) 
+        {
+            TempData["FundraiserName"] = FundraiserName;
+            TempData["Description"] = Description;
+            TempData["CurrentAmount"] = CurrentAmount;
+            TempData["Goal"] = Goal;
             return View();
         }
 
-        public IActionResult FundraiserListView() 
+        public IActionResult FundraiserListView(IFormCollection form) 
         {
+            string name = form["FundraiserName"];
+            TempData["FundraiserName"] = name;
             IEnumerable<Fundraiser_1> objFundraiserList = _db.Fundraiser_1s;
-
             return View(objFundraiserList);
         }
     }
