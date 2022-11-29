c66ee4ab24fb4ad13afaf7372d8004fd4ee956f9
babikirm <mbabikir2@gmail.com>
2022-11-29 15:05:55 -0600

    StartFunding

--- Controllers/StartFundraisingController.cs
+++ Controllers/StartFundraisingController.cs
@@ -1,9 +1,16 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CommerceProject.Data;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CommerceProject.Controllers
 {
     public class StartFundraisingController : Controller
     {
+        private readonly ApplicationDBContext _db;
+        public StartFundraisingController(ApplicationDBContext db)
+        {
+            _db = db;
+        }
+
         public IActionResult Index()
         {
             return View();
